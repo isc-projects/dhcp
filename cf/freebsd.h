dc7ee01d1fef208faa186bd7e9b1b22850388bcd
Ted Lemon <source@isc.org>
1996-05-20 00:37:54 +0000

    Fix copyright, INADDR_LOOPBACK; lose useless modemd time macros; SIN_LEN becomes SA_LEN

--- cf/freebsd.h
+++ cf/freebsd.h
@@ -14,15 +14,15 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of RadioMail Corporation nor the names of its
+ * 3. Neither the name of The Internet Software Consortium nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY RADIOMAIL CORPORATION AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * THIS SOFTWARE IS PROVIDED BY THE INTERNET SOFTWARE CONSORTIUM AND
+ * CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
  * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- * RADIOMAIL CORPORATION OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * THE INTERNET SOFTWARE CONSORTIUM OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
  * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -32,7 +32,7 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * This software was written for the Internet Software Consortium by Ted Lemon
- * under a contract with Vixie Labs.
+ * under a contract with Vixie Laboratories.
  */
 
 #define _ANSI_SOURCE
@@ -53,7 +53,7 @@ extern int h_errno;
 
 #include <net/if.h>
 #include <net/if_dl.h>
-#define INADDR_LOOPBACK ((u_long)0x7f000001)
+#define INADDR_LOOPBACK	((u_int32_t)0x7f000001)
 
 /* Varargs stuff... */
 #include <stdarg.h>
@@ -70,12 +70,8 @@ extern int h_errno;
 #include <sys/time.h>
 #define TIME time_t
 #define GET_TIME(x)	time ((x))
-#define TIME_DIFF(high, low)	 	(*(high) - *(low))
-#define SET_TIME(x, y)	(*(x) = (y))
-#define ADD_TIME(d, s1, s2) (*(d) = *(s1) + *(s2))
-#define SET_MAX_TIME(x)	(*(x) = INT_MAX)
 
-#define HAVE_SIN_LEN
+#define HAVE_SA_LEN
 
 #if defined (USE_DEFAULT_NETWORK)
 #  define USE_BPF
