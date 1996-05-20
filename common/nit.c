4a97f5b8fbcd0a2befad89c34e98a6f7cc51baec
Ted Lemon <source@isc.org>
1996-05-20 00:12:54 +0000

    Use distributed Berkeley network headers

--- common/nit.c
+++ common/nit.c
@@ -59,9 +59,9 @@ static char copyright[] =
 #include <net/packetfilt.h>
 
 #include <netinet/in_systm.h>
-#include <netinet/ip.h>
-#include <netinet/udp.h>
-#include <netinet/if_ether.h>
+#include "includes/netinet/ip.h"
+#include "includes/netinet/udp.h"
+#include "includes/netinet/if_ether.h"
 
 /* Called by get_interface_list for each interface that's discovered.
    Opens a packet filter for each interface and adds it to the select
