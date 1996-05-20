efc12bbc8f2103bfae857a1d0ce673a2a310c806
Ted Lemon <source@isc.org>
1996-05-20 00:11:03 +0000

    Fix copyright

--- includes/dhctoken.h
+++ includes/dhctoken.h
@@ -3,7 +3,8 @@
    Tokens for config file lexer and parser. */
 
 /*
- * Copyright (c) 1995 The Internet Software Consortium.  All rights reserved.
+ * Copyright (c) 1995, 1996 The Internet Software Consortium.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
