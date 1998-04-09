58dfdef0ea3914ee762c948add2305ade2c9171d
Ted Lemon <source@isc.org>
1998-04-09 04:36:25 +0000

    Fix copyright

--- includes/cf/linux.h
+++ includes/cf/linux.h
@@ -5,7 +5,8 @@
    Based on a configuration originally supplied by Jonathan Stone. */
 
 /*
- * Copyright (c) 1996 The Internet Software Consortium.  All rights reserved.
+ * Copyright (c) 1996, 1998 The Internet Software Consortium.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
