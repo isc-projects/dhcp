f90c6527fa4402647ef73b53d2b360b13d2d7661
Ted Lemon <source@isc.org>
1998-11-05 18:51:16 +0000

    Add some code to prevent linking in stuff we don't need (yuck).

--- relay/dhcrelay.c
+++ relay/dhcrelay.c
@@ -42,7 +42,7 @@
 
 #ifndef lint
 static char copyright[] =
-"$Id: dhcrelay.c,v 1.14 1998/06/25 18:37:45 mellon Exp $ Copyright (c) 1997 The Internet Software Consortium.  All rights reserved.\n";
+"$Id: dhcrelay.c,v 1.15 1998/11/05 18:51:16 mellon Exp $ Copyright (c) 1997 The Internet Software Consortium.  All rights reserved.\n";
 #endif /* not lint */
 
 #include "dhcpd.h"
@@ -231,7 +231,7 @@ int main (argc, argv, envp)
 	return 0;
 }
 
-void relay (ip, packbuf, length, from_port, from, hfrom)
+void relay (ip, packet, length, from_port, from, hfrom)
 	struct interface_info *ip;
 	struct dhcp_packet *packet;
 	int length;
@@ -251,7 +251,7 @@ void relay (ip, packbuf, length, from_port, from, hfrom)
 
 	/* If it's a bootreply, forward it to the client. */
 	if (packet -> op == BOOTREPLY) {
-#ifndef USE_FALLBACK
+#ifdef USE_FALLBACK
 		if (!(packet -> flags & htons (BOOTP_BROADCAST))) {
 			to.sin_addr = packet -> yiaddr;
 			to.sin_port = remote_port;
@@ -368,4 +368,10 @@ void bootp (packet)
 void dhcp (packet)
 	struct packet *packet;
 {
+}
+
+struct subnet *find_subnet (addr)
+	struct iaddr addr;
+{
+	return (struct subnet *)0;
 }
