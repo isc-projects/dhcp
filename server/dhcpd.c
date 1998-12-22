d79882ee1b6d0f8f4153050b49617a635e2ba960
Ted Lemon <source@isc.org>
1998-12-22 22:57:05 +0000

    Add request for contributions and URL.

--- server/dhcpd.c
+++ server/dhcpd.c
@@ -42,13 +42,15 @@
 
 #ifndef lint
 static char ocopyright[] =
-"$Id: dhcpd.c,v 1.45.2.5 1998/12/21 04:20:47 mellon Exp $ Copyright 1995, 1996, 1997, 1998 The Internet Software Consortium.";
+"$Id: dhcpd.c,v 1.45.2.6 1998/12/22 22:57:05 mellon Exp $ Copyright 1995, 1996, 1997, 1998 The Internet Software Consortium.";
 #endif
 
 static char copyright[] =
 "Copyright 1995, 1996, 1997, 1998 The Internet Software Consortium.";
 static char arr [] = "All rights reserved.";
 static char message [] = "Internet Software Consortium DHCP Server V2.0b1pl7 ";
+static char contrib [] = "Please contribute if you find this software useful.";
+static char url [] = "For info: http://www.isc.org/dhcp-contrib.html";
 
 #include "dhcpd.h"
 
@@ -169,6 +171,8 @@ int main (argc, argv, envp)
 		note (message);
 		note (copyright);
 		note (arr);
+		note (contrib);
+		note (url);
 	}
 
 	/* Default to the DHCP/BOOTP port. */
