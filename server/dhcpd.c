8bcdc603dc5427e2e7e3a7fdc74c20bd237cbe72
Ted Lemon <source@isc.org>
1999-04-09 17:42:07 +0000

    Update version.

--- server/dhcpd.c
+++ server/dhcpd.c
@@ -42,13 +42,13 @@
 
 #ifndef lint
 static char ocopyright[] =
-"$Id: dhcpd.c,v 1.45.2.25 1999/04/08 21:25:06 mellon Exp $ Copyright 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.";
+"$Id: dhcpd.c,v 1.45.2.26 1999/04/09 17:42:07 mellon Exp $ Copyright 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.";
 #endif
 
 static char copyright[] =
 "Copyright 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.";
 static char arr [] = "All rights reserved.";
-static char message [] = "Internet Software Consortium DHCP Server V2.0b1pl24";
+static char message [] = "Internet Software Consortium DHCP Server V2.0b1pl25";
 static char contrib [] = "Please contribute if you find this software useful.";
 static char url [] = "For info, please visit http://www.isc.org/dhcp-contrib.html";
 
