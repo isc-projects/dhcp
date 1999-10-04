49f5e70407e124b4ac4110b6b530c8a08af0e0b7
Ted Lemon <source@isc.org>
1999-10-04 23:12:11 +0000

    Prototypes for nsupdate code.

--- includes/dhcpd.h
+++ includes/dhcpd.h
@@ -1557,3 +1557,13 @@ struct auth_key *auth_key_lookup PROTO ((struct data_string *));
 void enter_failover_peer PROTO ((struct failover_peer *));
 struct failover_peer *find_failover_peer PROTO ((char *));
 
+/* nsupdate.c */
+
+char *ddns_rev_name PROTO ((struct lease *,
+			    struct lease_state *, struct packet *));
+char *ddns_fwd_name PROTO ((struct lease *,
+			    struct lease_state *, struct packet *));
+int nsupdateA PROTO ((char *, char *, u_int32_t, int));
+int nsupdatePTR PROTO ((char *, char *, u_int32_t, int));
+void nsupdate PROTO ((struct lease *,
+		      struct lease_state *, struct packet *, int));
