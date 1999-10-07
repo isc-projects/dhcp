9ed48e18d36be06a095caa35d5b1422e8e5cac92
Ted Lemon <source@isc.org>
1999-10-07 21:36:27 +0000

    Fix bugs in one-lease-per-client

--- server/dhcp.c
+++ server/dhcp.c
@@ -22,7 +22,7 @@
 
 #ifndef lint
 static char copyright[] =
-"$Id: dhcp.c,v 1.100.2.2 1999/08/19 18:57:09 mellon Exp $ Copyright (c) 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.  All rights reserved.\n";
+"$Id: dhcp.c,v 1.100.2.3 1999/10/07 21:36:27 mellon Exp $ Copyright (c) 1995, 1996, 1997, 1998, 1999 The Internet Software Consortium.  All rights reserved.\n";
 #endif /* not lint */
 
 #include "dhcpd.h"
@@ -896,7 +896,7 @@ void ack_lease (packet, lease, offer, when, msg)
 	   network interface, and will only ever remember one lease.   So
 	   if it sends a DHCPREQUEST, and doesn't get the lease, it's already
 	   forgotten about its old lease, so we can too. */
-	if (offer == DHCPREQUEST &&
+	if (packet -> packet_type == DHCPREQUEST &&
 	    (oc = lookup_option (&server_universe, state -> options,
 				 SV_ONE_LEASE_PER_CLIENT)) &&
 	    evaluate_boolean_option_cache (packet,
@@ -912,7 +912,7 @@ void ack_lease (packet, lease, offer, when, msg)
 					if (seek != lease &&
 					    seek -> ends > cur_time)
 						break;
-					seek = lease -> n_uid;
+					seek = seek -> n_uid;
 				}
 				if (seek) {
 					release_lease (seek);
@@ -927,7 +927,7 @@ void ack_lease (packet, lease, offer, when, msg)
 					if (seek != lease &&
 					    seek -> ends > cur_time)
 						break;
-					seek = lease -> n_hw;
+					seek = seek -> n_hw;
 				}
 				if (seek) {
 					release_lease (seek);
