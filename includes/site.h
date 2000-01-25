5092dd06017cc1a6c4f0496a757f28222c142509
Ted Lemon <source@isc.org>
2000-01-25 01:18:47 +0000

    Add malloc debugging defines.

--- includes/site.h
+++ includes/site.h
@@ -42,6 +42,26 @@
 
 /* #define DEBUG_CLASS_MATCHING */
 
+/* Define this if you want to track memory usage for the purpose of
+   noticing memory leaks quickly. */
+
+/* #define DEBUG_MEMORY_LEAKAGE */
+
+/* Define this if you want exhaustive (and very slow) checking of the
+   malloc pool for corruption. */
+
+/* #define DEBUG_MALLOC_POOL */
+
+/* Define this if you want to maintain a history of the last N operations
+   that changed reference counts on objects.   This can be used to debug
+   cases where an object is dereferenced too often, or not often enough. */
+
+/* #define DEBUG_RC_HISTORY */
+
+/* This is the number of history entries to maintain - by default, 256. */
+
+/* #define RC_HISTORY_MAX xxx */
+
 /* Define this if you want dhcpd to dump core when a non-fatal memory
    allocation error is detected (i.e., something that would cause a
    memory leak rather than a memory smash). */
