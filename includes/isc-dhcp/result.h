503be5526e9de8e420700c5a544a98f9b8848295
Ted Lemon <source@isc.org>
2001-05-19 07:31:10 +0000

    Add CONNRESET and UNKNOWNATTRIBUTE.

--- includes/isc-dhcp/result.h
+++ includes/isc-dhcp/result.h
@@ -93,11 +93,13 @@ typedef enum {
 	ISC_R_DESTADDRREQ = 64,
 	ISC_R_CROSSZONE = 65,
 	ISC_R_NO_TSIG = 66,
-	ISC_R_NOT_EQUAL = 67
+	ISC_R_NOT_EQUAL = 67,
+	ISC_R_CONNRESET = 68,
+	ISC_R_UNKNOWNATTRIBUTE = 69
 } isc_result_t;
 
 
-#define ISC_R_NRESULTS 			68	/* Number of results */
+#define ISC_R_NRESULTS 			70	/* Number of results */
 
 const char *		isc_result_totext(isc_result_t);
 isc_result_t		isc_result_register(unsigned int base,
