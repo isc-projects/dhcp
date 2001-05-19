353704da100401aa9bec074acb937a68c8ea65f6
Ted Lemon <source@isc.org>
2001-05-19 07:32:34 +0000

    If waiter gets a disconnect message, flag object as ready with ECONNRESET.

--- omapip/dispatch.c
+++ omapip/dispatch.c
@@ -573,6 +573,13 @@ isc_result_t omapi_waiter_signal_handler (omapi_object_t *h,
 		return ISC_R_SUCCESS;
 	}
 
+	if (!strcmp (name, "disconnect")) {
+		waiter = (omapi_waiter_object_t *)h;
+		waiter -> ready = 1;
+		waiter -> waitstatus = ISC_R_CONNRESET;
+		return ISC_R_SUCCESS;
+	}
+
 	if (h -> inner && h -> inner -> type -> signal_handler)
 		return (*(h -> inner -> type -> signal_handler)) (h -> inner,
 								  name, ap);
