00d83eb3fe5efae64949574c8a787d1e4697d187
Wendy <source@isc.org>
2001-09-20 18:49:31 +0000

    Added four commands to the TOKEN_HELP section: 	update 	unset <name> 	refresh 	remove

--- dhcpctl/omshell.c
+++ dhcpctl/omshell.c
@@ -209,6 +209,10 @@ int main (int argc, char **argv, char **envp)
 		    printf ("  set <name> = <value>\n");
 		    printf ("  create\n");
 		    printf ("  open\n");
+		    printf ("  update\n");
+		    printf ("  unset <name>\n");
+		    printf ("  refresh\n");
+		    printf ("  remove\n");
 		    skip_to_semi (cfile);
 		    break;
 		    
