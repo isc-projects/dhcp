f060020fdb2cb1b03f666a077807b834b36be6ca
Ted Lemon <source@isc.org>
2001-10-18 20:30:02 +0000

    Oops, add some more rc flags.

--- common/alloc.c
+++ common/alloc.c
@@ -43,7 +43,7 @@
 
 #ifndef lint
 static char copyright[] =
-"$Id: alloc.c,v 1.53.2.7 2001/10/18 20:09:34 mellon Exp $ Copyright (c) 1996-2000 The Internet Software Consortium.  All rights reserved.\n";
+"$Id: alloc.c,v 1.53.2.8 2001/10/18 20:30:02 mellon Exp $ Copyright (c) 1996-2000 The Internet Software Consortium.  All rights reserved.\n";
 #endif /* not lint */
 
 #include "dhcpd.h"
@@ -570,7 +570,7 @@ int binding_value_reference (ptr, src, file, line)
 	}
 	*ptr = src;
 	src -> refcnt++;
-	rc_register (file, line, ptr, src, src -> refcnt, 0);
+	rc_register (file, line, ptr, src, src -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
@@ -636,7 +636,7 @@ int fundef_reference (ptr, src, file, line)
 	}
 	*ptr = src;
 	src -> refcnt++;
-	rc_register (file, line, ptr, src, src -> refcnt, 0);
+	rc_register (file, line, ptr, src, src -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
@@ -701,7 +701,7 @@ int option_cache_reference (ptr, src, file, line)
 	}
 	*ptr = src;
 	src -> refcnt++;
-	rc_register (file, line, ptr, src, src -> refcnt, 0);
+	rc_register (file, line, ptr, src, src -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
@@ -745,7 +745,7 @@ int buffer_reference (ptr, bp, file, line)
 	}
 	*ptr = bp;
 	bp -> refcnt++;
-	rc_register (file, line, ptr, bp, bp -> refcnt, 0);
+	rc_register (file, line, ptr, bp, bp -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
@@ -775,7 +775,7 @@ int buffer_dereference (ptr, file, line)
 	}
 
 	(*ptr) -> refcnt--;
-	rc_register (file, line, ptr, *ptr, (*ptr) -> refcnt, 1);
+	rc_register (file, line, ptr, *ptr, (*ptr) -> refcnt, 1, RC_MISC);
 	if (!(*ptr) -> refcnt) {
 		dfree ((*ptr), file, line);
 	} else if ((*ptr) -> refcnt < 0) {
@@ -834,7 +834,7 @@ int dns_host_entry_reference (ptr, bp, file, line)
 	}
 	*ptr = bp;
 	bp -> refcnt++;
-	rc_register (file, line, ptr, bp, bp -> refcnt, 0);
+	rc_register (file, line, ptr, bp, bp -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
@@ -855,7 +855,7 @@ int dns_host_entry_dereference (ptr, file, line)
 	}
 
 	(*ptr) -> refcnt--;
-	rc_register (file, line, ptr, *ptr, (*ptr) -> refcnt, 1);
+	rc_register (file, line, ptr, *ptr, (*ptr) -> refcnt, 1, RC_MISC);
 	if (!(*ptr) -> refcnt)
 		dfree ((*ptr), file, line);
 	if ((*ptr) -> refcnt < 0) {
@@ -903,7 +903,8 @@ int option_state_allocate (ptr, file, line)
 		memset (*ptr, 0, size);
 		(*ptr) -> universe_count = universe_count;
 		(*ptr) -> refcnt = 1;
-		rc_register (file, line, ptr, *ptr, (*ptr) -> refcnt, 0);
+		rc_register (file, line,
+			     ptr, *ptr, (*ptr) -> refcnt, 0, RC_MISC);
 		return 1;
 	}
 	return 0;
@@ -933,7 +934,7 @@ int option_state_reference (ptr, bp, file, line)
 	}
 	*ptr = bp;
 	bp -> refcnt++;
-	rc_register (file, line, ptr, bp, bp -> refcnt, 0);
+	rc_register (file, line, ptr, bp, bp -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
@@ -957,7 +958,7 @@ int option_state_dereference (ptr, file, line)
 	options = *ptr;
 	*ptr = (struct option_state *)0;
 	--options -> refcnt;
-	rc_register (file, line, ptr, options, options -> refcnt, 1);
+	rc_register (file, line, ptr, options, options -> refcnt, 1, RC_MISC);
 	if (options -> refcnt > 0)
 		return 1;
 
@@ -1021,7 +1022,7 @@ int executable_statement_reference (ptr, bp, file, line)
 	}
 	*ptr = bp;
 	bp -> refcnt++;
-	rc_register (file, line, ptr, bp, bp -> refcnt, 0);
+	rc_register (file, line, ptr, bp, bp -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
@@ -1103,7 +1104,7 @@ int packet_reference (ptr, bp, file, line)
 	}
 	*ptr = bp;
 	bp -> refcnt++;
-	rc_register (file, line, ptr, bp, bp -> refcnt, 0);
+	rc_register (file, line, ptr, bp, bp -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
@@ -1127,7 +1128,7 @@ int packet_dereference (ptr, file, line)
 	packet = *ptr;
 	*ptr = (struct packet *)0;
 	--packet -> refcnt;
-	rc_register (file, line, ptr, packet, packet -> refcnt, 1);
+	rc_register (file, line, ptr, packet, packet -> refcnt, 1, RC_MISC);
 	if (packet -> refcnt > 0)
 		return 1;
 
@@ -1217,7 +1218,7 @@ int dns_zone_reference (ptr, bp, file, line)
 	}
 	*ptr = bp;
 	bp -> refcnt++;
-	rc_register (file, line, ptr, bp, bp -> refcnt, 0);
+	rc_register (file, line, ptr, bp, bp -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
@@ -1278,7 +1279,7 @@ int binding_scope_reference (ptr, bp, file, line)
 	}
 	*ptr = bp;
 	bp -> refcnt++;
-	rc_register (file, line, ptr, bp, bp -> refcnt, 0);
+	rc_register (file, line, ptr, bp, bp -> refcnt, 0, RC_MISC);
 	return 1;
 }
 
