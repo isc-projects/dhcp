ef87118a9ec651864562c0f4a7917bc33c663228
David Hankins <dhankins@isc.org>
2007-04-26 19:33:15 +0000

    - dhclient now closes its descriptor to dhclient.leases prior to executing   dhclient-script.  Thanks to a patch from Tomas Pospisek. [ISC-Bugs #16728]

--- client/dhclient.c
+++ client/dhclient.c
@@ -32,7 +32,7 @@
 
 #ifndef lint
 static char ocopyright[] =
-"$Id: dhclient.c,v 1.129.2.34 2006/08/22 15:13:57 dhankins Exp $ Copyright (c) 2004-2006 Internet Systems Consortium.  All rights reserved.\n";
+"$Id: dhclient.c,v 1.129.2.35 2007/04/26 19:33:15 dhankins Exp $ Copyright (c) 2004-2006 Internet Systems Consortium.  All rights reserved.\n";
 #endif /* not lint */
 
 #include "dhcpd.h"
@@ -2621,6 +2621,11 @@ int script_go (client)
 			wstatus = 0;
 		}
 	} else {
+		/* We don't want to pass an open file descriptor for
+		 * dhclient.leases when executing dhclient-script.
+		 */
+		if (leaseFile != NULL)
+			fclose(leaseFile);
 		execve (scriptName, argv, envp);
 		log_error ("execve (%s, ...): %m", scriptName);
 		exit (0);
