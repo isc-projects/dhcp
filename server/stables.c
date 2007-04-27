29067c177347e67e9c6b21d8418bc049128c4c7e
Evan Hunt <each@isc.org>
2007-04-27 22:48:10 +0000

    A new server config option "fqdn-reply" specifies whether the server should send out option 81 (FQDN).  Defaults to "on".  If set to "off", the FQDN option is not sent, even if the client requested it.  This is needed because some clients misbehave otherwise. [rt16624]

--- server/stables.c
+++ server/stables.c
@@ -34,7 +34,7 @@
 
 #ifndef lint
 static char copyright[] =
-"$Id: stables.c,v 1.33.12.4 2007/04/03 16:47:32 dhankins Exp $ Copyright (c) 2004-2007 Internet Systems Consortium.  All rights reserved.\n";
+"$Id: stables.c,v 1.33.12.5 2007/04/27 22:48:10 each Exp $ Copyright (c) 2004-2007 Internet Systems Consortium.  All rights reserved.\n";
 #endif /* not lint */
 
 #include "dhcpd.h"
@@ -237,6 +237,7 @@ static struct option server_options[] = {
 	{ "leasequery", "f",			&server_universe,  49, 1 },
 	{ "adaptive-lease-time-threshold", "B",	&server_universe,  50, 1 },
 	{ "do-reverse-updates", "f",		&server_universe,  51, 1 },
+	{ "fqdn-reply", "f",			&server_universe,  52, 1 },
 	{ NULL, NULL, NULL, 0, 0 }
 };
 
