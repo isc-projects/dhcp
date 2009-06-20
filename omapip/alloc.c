25660a8c0f229089168595f33f8f7f56d3603d30
David Hankins <dhankins@isc.org>
2009-06-20 02:02:28 +0000

    Missing copyright updates.

--- omapip/alloc.c
+++ omapip/alloc.c
@@ -4,7 +4,7 @@
    protocol... */
 
 /*
- * Copyright (c) 2004-2007 by Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (c) 2004-2007,2009 by Internet Systems Consortium, Inc. ("ISC")
  * Copyright (c) 1999-2003 by Internet Software Consortium
  *
  * Permission to use, copy, modify, and distribute this software for any
