32fd12cc747eb3c8dc9e13508e84a46d78b2f763
Paul Selkirk <pselkirk@isc.org>
2009-07-16 22:01:26 +0000

    update copyright

--- common/discover.c
+++ common/discover.c
@@ -3,7 +3,7 @@
    Find and identify the network interfaces. */
 
 /*
- * Copyright (c) 2004-2008 by Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (c) 2004-2009 by Internet Systems Consortium, Inc. ("ISC")
  * Copyright (c) 1995-2003 by Internet Software Consortium
  *
  * Permission to use, copy, modify, and distribute this software for any
