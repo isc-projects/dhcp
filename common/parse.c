86f1d4b72395f9102b974cdd1fb4e40599530a64
David Hankins <dhankins@isc.org>
2009-07-16 18:02:28 +0000

    - Versions 3.0.x syntax with multiple name->code option definitions is now   supported.  Note that, similarly to 3.0.x, for by-code lookups only the   last option definition is used.  [ISC-Bugs #17613]

--- common/parse.c
+++ common/parse.c
@@ -1757,9 +1757,18 @@ int parse_option_code_definition (cfile, option)
 	oldopt = NULL;
 	option_code_hash_lookup(&oldopt, option->universe->code_hash,
 				&option->code, 0, MDL);
-	if (oldopt) {
+	if (oldopt != NULL) {
+		/*
+		 * XXX: This illegalizes a configuration syntax that was
+		 * valid in 3.0.x, where multiple name->code mappings are
+		 * given, but only one code->name mapping survives.  It is
+		 * unclear what can or should be done at this point, but it
+		 * seems best to retain 3.0.x behaviour for upgrades to go
+		 * smoothly.
+		 *
 		option_name_hash_delete(option->universe->name_hash,
 					oldopt->name, 0, MDL);
+		 */
 		option_code_hash_delete(option->universe->code_hash,
 					&oldopt->code, 0, MDL);
 
