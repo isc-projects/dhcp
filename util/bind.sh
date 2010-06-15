795b3525a1c7fb43156e7f23899becd782cc6f52
David Hankins <dhankins@isc.org>
2010-06-15 18:41:51 +0000

    BIND 9 export library version for 4.2.0rc1/4.2.0 final to v9_7_1 final.

--- util/bind.sh
+++ util/bind.sh
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: bind.sh,v 1.12 2010/06/03 16:25:33 dhankins Exp $
+# $Id: bind.sh,v 1.13 2010/06/15 18:41:51 dhankins Exp $
 
 # Get the bind distribution for the libraries
 # This script is used to build the DHCP distribution and shouldn't be shipped
@@ -45,7 +45,8 @@ case $# in
 	### For ease of use, this records the sticky tag of versions
 	### released with each point release.
 	###
-	4.2.0b2|4.2.0) BINDTAG=v9_7_1rc1 ;;
+	4.2.0rc1|4.2.0) BINDTAG=v9_7_1 ;;
+	4.2.0b2) BINDTAG=v9_7_1rc1 ;;
 	4.2.0b1) BINDTAG=v9_7_0_P1 ;;
 	4.2.0a2|4.2.0a1) BINDTAG=v9_7_0b3 ;;
 	*) echo "bind.sh: unsupported version: $1" >&2
