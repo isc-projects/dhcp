06919067a3470fd30fba6103ea959559aa6383f2
David Hankins <dhankins@isc.org>
2010-09-17 00:05:44 +0000

    - Version to 4.0.3b1. - README/RELNOTES++ - Copyrights to 2010.

--- server/omapi.c
+++ server/omapi.c
@@ -3,7 +3,8 @@
    OMAPI object interfaces for the DHCP server. */
 
 /*
- * Copyright (c) 2004-2007,2009 by Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (c) 2009-2010 by Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (c) 2004-2007 by Internet Systems Consortium, Inc. ("ISC")
  * Copyright (c) 1999-2003 by Internet Software Consortium
  *
  * Permission to use, copy, modify, and distribute this software for any
