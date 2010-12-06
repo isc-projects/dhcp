01910481cd40958a03bdc9569eddd4bccb172eca
Mark Andrews <marka@isc.org>
2010-12-06 02:00:25 +0000

    dns_tsec_create now takes the address of the dstkey pointer

--- omapip/auth.c
+++ omapip/auth.c
@@ -114,7 +114,7 @@ isc_result_t omapi_auth_key_enter (omapi_auth_key_t *a)
 				     &dstkey);
 	if (status == ISC_R_SUCCESS) {
 		status = dns_tsec_create(dhcp_gbl_ctx.mctx, dns_tsectype_tsig,
-					 dstkey, &a->tsec_key);
+					 &dstkey, &a->tsec_key);
 	}
 	if (status != ISC_R_SUCCESS) {
 		if (dstkey != NULL) {
