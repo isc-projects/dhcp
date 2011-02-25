b7b43092cfc88d7680249b9e419b386486129983
Shawn Routhier <sar@isc.org>
2011-02-25 20:08:55 +0000

    Add tag for 4.2.1 => 9.8.0

--- util/bind.sh
+++ util/bind.sh
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: bind.sh,v 1.16 2011/02/18 19:50:45 sar Exp $
+# $Id: bind.sh,v 1.17 2011/02/25 20:08:55 sar Exp $
 
 # Get the bind distribution for the libraries
 # This script is used to build the DHCP distribution and shouldn't be shipped
@@ -45,6 +45,7 @@ case $# in
 	### For ease of use, this records the sticky tag of versions
 	### released with each point release.
 	###
+	4.2.1) BINDTAG=v9_8_0 ;;
 	4.2.1rc1) BINDTAG=v9_8_0rc1 ;;
 	4.2.1b1) BINDTAG=v9_8_0b1 ;;
 	4.2.0rc1|4.2.0) BINDTAG=v9_7_1 ;;
