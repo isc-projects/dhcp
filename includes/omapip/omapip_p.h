c7da94b5a48eaa0003ef650f560a6bb3493469eb
Shawn Routhier <sar@isc.org>
2012-02-16 22:07:17 +0000

    Add a compile time check for the presence of the noreturn attribute and use it for log_fatal if it's available.  This will help code checking programs to eliminate false positives. [ISC-Bugs 27539]

--- includes/omapip/omapip_p.h
+++ includes/omapip/omapip_p.h
@@ -289,7 +289,7 @@ extern int log_perror;
 extern void (*log_cleanup) (void);
 
 void log_fatal (const char *, ...)
-	__attribute__((__format__(__printf__,1,2)));
+	__attribute__((__format__(__printf__,1,2))) ISC_DHCP_NORETURN;
 int log_error (const char *, ...)
 	__attribute__((__format__(__printf__,1,2)));
 int log_info (const char *, ...)
