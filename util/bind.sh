5f272feddf58044745a30b8f3e0cf94395bc4cb1
Shawn Routhier <sar@isc.org>
2012-04-10 23:52:06 +0000

    Checkout the bind9 code	from git instead of cvs.  This version avoids cloning	 the repo.

--- util/bind.sh
+++ util/bind.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2009-2011  Internet Systems Consortium, Inc. ("ISC")
+# Copyright (C) 2009-2012  Internet Systems Consortium, Inc. ("ISC")
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-# $Id: bind.sh,v 1.26 2012/04/06 02:31:32 sar Exp $
+# $Id: bind.sh,v 1.27 2012/04/10 23:52:06 sar Exp $
 
 # Get the bind distribution for the libraries
 # This script is used to build the DHCP distribution and shouldn't be shipped
@@ -39,12 +39,13 @@ case $# in
 	### tarball with vastly different values.  So the version.tmp can not
 	### be used to chdir down into the directory that is unpacked.
 	###
-	v4_2) noSNAP=snapshot BINDTAG=v9_7 ;;
+	v4_2) noSNAP=snapshot BINDTAG=v9_8 ;;
 	HEAD|v[0-9]_[0-9].*) noSNAP=snapshot BINDTAG=HEAD ;;
 	###
 	### For ease of use, this records the sticky tag of versions
 	### released with each point release.
 	###
+	4.2.4b1) BINDTAG=v9_8_2 ;;
 	4.2.3-P1|4.2.3-P2) BINDTAG=v9_8_1_P1 ;;
 	4.2.3rc1|4.2.3) BINDTAG=v9_8_1 ;;
 	4.2.2rc1|4.2.2) BINDTAG=v9_8_0_P4 ;;
@@ -73,12 +74,17 @@ mkdir $binddir
 cp util/Makefile.bind bind/Makefile
 cd $binddir
 
+# Get the bind version file and move it to version.tmp
+git archive --format tar --remote=cvs.isc.org:/proj/git/prod/bind9.git $BINDTAG version | tar xf -
+mv version version.tmp
+
 # Get the bind release kit shell script
-cvs checkout -p -r $BINDTAG bind9/util/kit.sh > kit.sh
+git archive --format tar --remote=cvs.isc.org:/proj/git/prod/bind9.git  master:util/ | tar xf - kit.sh
 
 # Create the bind tarball, which has the side effect of
 # setting up the bind directory we will use for building
 # the export libraries
+echo Creating tarball for $BINDTAG
 sh kit.sh $SNAP $BINDTAG $binddir
 
 . ./version.tmp
@@ -88,12 +94,6 @@ bindsrcdir=bind-$version
 mm=${MAJORVER}.${MINORVER}
 
 # move the tar file to a known place for use by the make dist command
+echo Moving tar file to bind.tar.gz for distribution
 mv bind-${mm}*.tar.gz bind.tar.gz
-
-# temporary hack to allow testing when using snapshots
-#mv $binddir/bind-9.7* $binddir/$bindsrcdir
-
-# Run the script to build and install the export libraries
-# Let make do this now.
-#sh $topdir/util/bindlib.sh $binddir $bindsrcdir
 
