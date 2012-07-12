7d83ee14f72eeaee2856f39b3de2663dfce6d676
Shawn Routhier <sar@isc.org>
2012-07-12 13:20:03 -0700

    [rt30024] This is a commit message Fix a pair of memory leaks.

--- common/options.c
+++ common/options.c
@@ -2397,6 +2397,8 @@ prepare_option_buffer(struct universe *universe, struct buffer *bp,
 
 	/* And let go of our references. */
       cleanup:
+	if (lbp != NULL)
+		buffer_dereference(&lbp, MDL);
 	option_dereference(&option, MDL);
 
 	return status;
