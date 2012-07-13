5e1ef01178527149e5673002dbdc3be18046c77e
Shawn Routhier <sar@isc.org>
2012-07-12 23:00:44 -0700

    [rt30024] Move the line that zeros the packet_oro variable to be earlier so it is properly cleared if we exit early.

--- server/dhcpv6.c
+++ server/dhcpv6.c
@@ -1253,6 +1253,8 @@ lease_to_client(struct data_string *reply_ret,
 	isc_boolean_t no_resources_avail = ISC_FALSE;
 #endif
 
+	memset(&packet_oro, 0, sizeof(packet_oro));
+
 	/* Locate the client.  */
 	if (shared_network_from_packet6(&reply.shared,
 					packet) != ISC_R_SUCCESS)
@@ -1275,7 +1277,6 @@ lease_to_client(struct data_string *reply_ret,
 	 * Get the ORO from the packet, if any.
 	 */
 	oc = lookup_option(&dhcpv6_universe, packet->options, D6O_ORO);
-	memset(&packet_oro, 0, sizeof(packet_oro));
 	if (oc != NULL) {
 		if (!evaluate_option_cache(&packet_oro, packet, 
 					   NULL, NULL, 
