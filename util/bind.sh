dc9ea729746e1e1d34b1a7bc93b5beab0509108d
Shawn Routhier <sar@isc.org>
2014-01-27 21:20:42 -0800

    [v4_2] bind version

--- util/bind.sh
+++ util/bind.sh
@@ -55,6 +55,7 @@ case $# in
 	### For ease of use, this records the sticky tag of versions
 	### released with each point release.
 	###
+	4.2.6rc1) BINDTAG=v9_9_5rc2 ;;
 	4.2.6b1) BINDTAG=v9_9_5rc1 ;;
 	4.2.5b1|4.2.5rc1|4.2.5) BINDTAG=v9_8_4_P1 ;;
 	4.2.4rc2|4.2.4) BINDTAG=v9_8_3 ;;
