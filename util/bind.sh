c09c9e3eba2925b4119d8b3a383bdbecba810bcb
Shawn Routhier <sar@isc.org>
2014-07-28 16:11:06 -0700

    [v4_2] Update for version 4.2.7rc1

--- util/bind.sh
+++ util/bind.sh
@@ -58,7 +58,7 @@ case $# in
 	### For ease of use, this records the sticky tag of versions
 	### released with each point release.
 	###
-	4.2.7b1) BINDTAG=v9_9_5_P1 ;;
+	4.2.7b1|4.2.7rc1) BINDTAG=v9_9_5_P1 ;;
 	4.2.6) BINDTAG=v9_9_5 ;;
 	4.2.6rc1) BINDTAG=v9_9_5rc2 ;;
 	4.2.6b1) BINDTAG=v9_9_5rc1 ;;
