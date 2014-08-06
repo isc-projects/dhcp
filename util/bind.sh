906ec9a72a07aa06300fd73e2d3158708b54f685
Shawn Routhier <sar@isc.org>
2014-08-06 15:38:59 -0700

    [master] Updates for 4.3.1 final

--- util/bind.sh
+++ util/bind.sh
@@ -59,7 +59,7 @@ case $# in
 	### For ease of use, this records the sticky tag of versions
 	### released with each point release.
 	###
-	4.3.1b1|4.3.1rc1) BINDTAG=v9_9_5_P1 ;;
+	4.3.1b1|4.3.1rc1|4.3.1) BINDTAG=v9_9_5_P1 ;;
 	4.3.0) BINDTAG=v9_9_5 ;;
 	4.3.0rc1) BINDTAG=v9_9_5rc2 ;;
 	4.3.0b1) BINDTAG=v9_9_5rc1 ;;
