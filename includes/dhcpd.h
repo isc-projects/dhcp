c63e45951d836a224fb7edd31ca2faded902a04f
Thomas Markwalder <tmark@isc.org>
2014-11-05 07:17:37 -0500

    [v4_1_esv] Host declaration name can now be used in DNS forward name
        Merges in rt21323.

--- includes/dhcpd.h
+++ includes/dhcpd.h
@@ -2117,6 +2117,8 @@ int option_state_reference (struct option_state **,
 			    struct option_state *, const char *, int);
 int option_state_dereference (struct option_state **,
 			      const char *, int);
+int data_string_new(struct data_string *, const char *, unsigned int,
+		    const char *, int);
 void data_string_copy(struct data_string *, const struct data_string *,
 		      const char *, int);
 void data_string_forget (struct data_string *, const char *, int);
