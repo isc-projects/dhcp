ae71c6021310c1fd34377fefcda7696cfee363c6
Thomas Markwalder <tmark@isc.org>
2016-08-26 16:37:17 -0400

    [v4_1_esv] Pre-release tag prep for 4.1-ESV-R14b1: version #, copyright etc...

--- common/discover.c
+++ common/discover.c
@@ -3,7 +3,7 @@
    Find and identify the network interfaces. */
 
 /*
- * Copyright (c) 2013-2014 by Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (c) 2013-2016 by Internet Systems Consortium, Inc. ("ISC")
  * Copyright (c) 2004-2009,2011 by Internet Systems Consortium, Inc. ("ISC")
  * Copyright (c) 1995-2003 by Internet Software Consortium
  *
