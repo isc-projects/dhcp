7eeb48507d308ed8f9b89aba871d77fb3c3850f2
Tomek Mrugalski <tomasz@isc.org>
2017-06-28 14:26:27 +0200

    [19430] Updated copyrights
    # Conflicts:
    #	omapip/errwarn.c

--- omapip/errwarn.c
+++ omapip/errwarn.c
@@ -4,7 +4,7 @@
 
 /*
  * Copyright (c) 1995 RadioMail Corporation.
- * Copyright (c) 2004-2016 by Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (c) 2004-2017 by Internet Systems Consortium, Inc. ("ISC")
  * Copyright (c) 1996-2003 by Internet Software Consortium
  *
  * Permission to use, copy, modify, and distribute this software for any
